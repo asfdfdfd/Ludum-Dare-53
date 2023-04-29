f416fc65ba1cc52a1d2e7a08166fb468d1bc42a9
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2023-04-29 23:56:32 +0300

    Birbs.

---
+++ Assets/Game/Scripts/RoadsideSpawnerController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RoadsideSpawnerController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _prefabs;
+
+    [SerializeField] private float _spawnPauseSecondsMin = 1.0f;
+    [SerializeField] private float _spawnPauseSecondsMax = 5.0f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    private IEnumerator SpawnCoroutine()
+    {
+        while (true)
+        {
+            float spawnPauseSeconds = Random.Range(_spawnPauseSecondsMin, _spawnPauseSecondsMax);
+            
+            yield return new WaitForSeconds(spawnPauseSeconds);
+
+            int prefabIndex = Random.Range(0, _prefabs.Count);
+
+            Instantiate(_prefabs[prefabIndex], transform.position, Quaternion.identity);
+        }
+    }
+}
